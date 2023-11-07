ca4af6fa116dc6e2ead068fcf5a35846f2c6e287
Flavianthepavian <flavianzuellig@hotmail.com>
2023-11-07 11:09:05 +0100

    Make StudentLogin resizeable

--- src/sites/StudentLogin/StudentLogin.tsx
+++ src/sites/StudentLogin/StudentLogin.tsx
@@ -11,25 +11,44 @@ export default function StudentLogin() {
         <div className="studentLoginPageContainer">
             <div className="studentLoginContainer">
                 <h3 className="studentLoginTitle">Schüler Login</h3>
-                <h5>Benutzername</h5>
-                <StyledTextInput
-                    value={username}
-                    size={"medium"}
-                    onChange={(result) => setUsername(result)}
-                    placeholder={"Gib deinen Benutzernamen ein..."}
-                />
-                <h5>Passwort</h5>
-                <StyledTextInput
-                    value={password}
-                    size={"medium"}
-                    password
-                    onChange={(result) => setPassword(result)}
-                    placeholder={"Gib dein Passwort ein..."}
-                />
-                <div className={"forgotPassword"}>
-                    <StyledLink text={"Passwort vergessen?"} to={"/resetPassword"} padding={"big"}></StyledLink>
+                <div className="formContainer">
+                    <h5 className="studentLoginSubTitle">Benutzername</h5>
+                    <StyledTextInput
+                        value={username}
+                        size={"medium"}
+                        onChange={(result) => setUsername(result)}
+                        placeholder={"Gib deinen Benutzernamen ein..."}
+                        style={{ height: "15%" }}
+                    />
+                    <h5 className="studentLoginSubTitle">Passwort</h5>
+                    <StyledTextInput
+                        value={password}
+                        size={"medium"}
+                        password
+                        onChange={(result) => setPassword(result)}
+                        placeholder={"Gib dein Passwort ein..."}
+                        style={{ height: "15%" }}
+                    />
                 </div>
-                <StyledButton size={"medium"} hover={{position: false, shadow: true, underline: false}} text={"Einloggen"} type={"gradient"}/>
+                <div className="submissionContainer">
+                    <div className={"forgotPassword"}>
+                        <StyledLink
+                            text={"Passwort vergessen?"}
+                            to={"/resetPassword"}
+                            padding={"big"}
+                        ></StyledLink>
+                    </div>
+                    <StyledButton
+                        size={"medium"}
+                        hover={{
+                            position: false,
+                            shadow: true,
+                            underline: false,
+                        }}
+                        text={"Einloggen"}
+                        type={"gradient"}
+                    />
+                </div>
             </div>
         </div>
     );
