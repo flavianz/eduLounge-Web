5f14147297e6077ded805e174a1f7b0f24be28c8
Flavianthepavian <flavianzuellig@hotmail.com>
2023-11-07 13:58:46 +0100

    Base Component for StudentDashboardOverview

---
+++ src/sites/StudentDashboard/StudentDashboardOverview/StudentDashboardOverview.tsx
@@ -0,0 +1,10 @@
+import "./StudentDashboardOverview.css";
+import StudentDashboardMenu from "../../../components/StudentDashboard/StudentDashboardMenu/StudentDashboardMenu";
+
+export default function StudentDashboardOverview() {
+    return (
+        <div className="studentDashboardMenuContainer">
+            <StudentDashboardMenu />
+        </div>
+    );
+}
