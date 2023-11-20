1dccb0470869992994dfb372c0fb835636a8dd3f
Flavianthepavian <flavianzuellig@hotmail.com>
2023-11-20 13:28:43 +0100

    Add search bar

--- src/components/StudentDashboard/StudentDashboardHeader/StudentDashboardHeader.tsx
+++ src/components/StudentDashboard/StudentDashboardHeader/StudentDashboardHeader.tsx
@@ -1,15 +1,27 @@
 import "./StudentDashboardHeader.css";
 import SVGIcon from "../../../assets/images/svg/svg";
+import { sites } from "../../../constants";
 
-export default function StudentDashboardHeader() {
+export default function StudentDashboardHeader({ pageID }: { pageID: sites }) {
+    const titles = {
+        overview: "Übersicht",
+        subjects: "Fächer",
+        timetable: "Stundenplan",
+        absences: "Absenzen",
+    };
     return (
         <div className="studentDashboardHeaderContainer">
-            <h3 className="studentDashboardHeaderTitle">Übersicht</h3>
+            <h3 className="studentDashboardHeaderTitle">{titles[pageID]}</h3>
             <div className="studentDashboardHeaderSearchBarContainer">
                 <input
                     type="text"
+                    placeholder={"Suche..."}
                     className={"studentDashboardHeaderSearchBarInput"}
                 />
+                <SVGIcon
+                    id={"lens"}
+                    className={"studentDashboardHeaderSearchBarIcon"}
+                ></SVGIcon>
             </div>
             <div className="studentDashboardHeaderIconContainer">
                 <SVGIcon
