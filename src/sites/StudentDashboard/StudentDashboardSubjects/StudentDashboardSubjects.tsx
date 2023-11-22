f1fad51e38ac530bdcca314cc0a28b40829f921d
Flavianthepavian <flavianzuellig@hotmail.com>
2023-11-22 14:47:04 +0100

    Add raw structure

--- src/sites/StudentDashboard/StudentDashboardSubjects/StudentDashboardSubjects.tsx
+++ src/sites/StudentDashboard/StudentDashboardSubjects/StudentDashboardSubjects.tsx
@@ -1,111 +1,10 @@
 import "./StudentDashboardSubjects.css";
 import StudentDashboardMenu from "../../../components/StudentDashboard/StudentDashboardMenu/StudentDashboardMenu";
 import StudentDashboardHeader from "../../../components/StudentDashboard/StudentDashboardHeader/StudentDashboardHeader";
+import { subjects } from "../../../scripts/dummy";
+import SVGIcon from "../../../assets/images/svg/svg";
 
 export default function StudentDashboardOverview() {
-    const subjects: {
-        [key: string]: {
-            teacher: {
-                name: string;
-            };
-            average: string;
-            klassen: string[];
-            name: string;
-            tests: {
-                [key: string]: {
-                    name: string;
-                    start: number;
-                    end: number;
-                    topics: string;
-                    createdAt: number;
-                    mark: number;
-                    type: "project" | "presentation" | "exam";
-                    weight: number;
-                };
-            };
-        };
-    } = {
-        bg: {
-            teacher: {
-                name: "Anja Messerli",
-            },
-            average: "4.5",
-            name: "Bildnerisches Gestalten",
-            klassen: ["1a, 1i"],
-            tests: {
-                "0": {
-                    name: "Bleistift",
-                    start: Date.now(),
-                    end: Date.now(),
-                    topics: "-Bleistifte\n-Zeichnungen",
-                    createdAt: Date.now(),
-                    type: "project",
-                    mark: 6,
-                    weight: 0.5,
-                },
-                "1": {
-                    name: "Vortrag",
-                    start: Date.now(),
-                    end: Date.now(),
-                    topics: "-Bleistifte\n-Zeichnungen",
-                    createdAt: Date.now(),
-                    type: "presentation",
-                    mark: 5,
-                    weight: 1,
-                },
-                "2": {
-                    name: "Farbenlehre",
-                    start: Date.now(),
-                    end: Date.now(),
-                    topics: "-Bleistifte\n-Zeichnungen",
-                    createdAt: Date.now(),
-                    type: "exam",
-                    mark: 4.5,
-                    weight: 1,
-                },
-            },
-        },
-        history: {
-            teacher: {
-                name: "Anja Messerli",
-            },
-            average: "4.5",
-            name: "Bildnerisches Gestalten",
-            klassen: ["1a, 1i"],
-            tests: {
-                "0": {
-                    name: "Bleistift",
-                    start: Date.now(),
-                    end: Date.now(),
-                    topics: "-Bleistifte\n-Zeichnungen",
-                    createdAt: Date.now(),
-                    type: "project",
-                    mark: 6,
-                    weight: 0.5,
-                },
-                "1": {
-                    name: "Vortrag",
-                    start: Date.now(),
-                    end: Date.now(),
-                    topics: "-Bleistifte\n-Zeichnungen",
-                    createdAt: Date.now(),
-                    type: "presentation",
-                    mark: 5,
-                    weight: 1,
-                },
-                "2": {
-                    name: "Farbenlehre",
-                    start: Date.now(),
-                    end: Date.now(),
-                    topics: "-Bleistifte\n-Zeichnungen",
-                    createdAt: Date.now(),
-                    type: "exam",
-                    mark: 4.5,
-                    weight: 1,
-                },
-            },
-        },
-    };
     return (
         <div className="studentDashboardContainer">
             <StudentDashboardMenu pageID={"subjects"} />
@@ -122,6 +21,12 @@ export default function StudentDashboardOverview() {
                                 <p className="studentDashboardSubjectName">
                                     {subject.name}
                                 </p>
+                                <SVGIcon
+                                    id={"details"}
+                                    className={
+                                        "studentDashboardSubjectDetailsIcon"
+                                    }
+                                />
                                 <p className={"studentDashboardSubjectAverage"}>
                                     {subject.average}
                                 </p>
