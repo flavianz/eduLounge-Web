01470879d7c33401e836099e086c4f680d448a5c
Flavianthepavian <survivallist.net@gmail.com>
2023-11-22 23:36:11 +0100

    Make subjects details openable and add nav to details button

--- src/sites/StudentDashboard/StudentDashboardSubjects/StudentDashboardSubjects.tsx
+++ src/sites/StudentDashboard/StudentDashboardSubjects/StudentDashboardSubjects.tsx
@@ -3,8 +3,12 @@ import StudentDashboardMenu from "../../../components/StudentDashboard/StudentDa
 import StudentDashboardHeader from "../../../components/StudentDashboard/StudentDashboardHeader/StudentDashboardHeader";
 import { subjects } from "../../../scripts/dummy";
 import SVGIcon from "../../../assets/images/svg/svg";
+import { useState } from "react";
+import { useNavigate } from "react-router";
 
 export default function StudentDashboardOverview() {
+    const [expandedSubjects, setExpandedSubjects] = useState<string[]>([]);
+    const navigate = useNavigate();
     return (
         <div className="studentDashboardContainer">
             <StudentDashboardMenu pageID={"subjects"} />
@@ -13,23 +17,89 @@ export default function StudentDashboardOverview() {
                 <div className="studentDashboardSubjectsDataContainer">
                     {Object.keys(subjects).map((id, key) => {
                         const subject = subjects[id];
-                        return (
+                        const expanded = expandedSubjects.includes(id);
+                        return !expanded ? (
                             <div
-                                className="studentDashboardSubjectContainer"
-                                key={key}
+                                className={"studentDashboardSubjectContainer"}
+                                onClick={() => {
+                                    setExpandedSubjects([
+                                        ...expandedSubjects,
+                                        id,
+                                    ]);
+                                }}
                             >
                                 <p className="studentDashboardSubjectName">
                                     {subject.name}
                                 </p>
-                                <SVGIcon
-                                    id={"details"}
-                                    className={
-                                        "studentDashboardSubjectDetailsIcon"
-                                    }
-                                />
+                                <div
+                                    onClick={() => {
+                                        navigate(
+                                            "/students/dashboard/subjects/details?s=" +
+                                                id
+                                        );
+                                    }}
+                                    className="studentDashboardSubjectDetailsIconContainer"
+                                >
+                                    <SVGIcon
+                                        id={"details"}
+                                        className={
+                                            "studentDashboardSubjectDetailsIcon"
+                                        }
+                                    />
+                                </div>
                                 <p className={"studentDashboardSubjectAverage"}>
                                     {subject.average}
                                 </p>
+                            </div>
+                        ) : (
+                            <div
+                                className={
+                                    "studentDashboardSubjectContainer" +
+                                    (expanded
+                                        ? " studentDashboardSubjectWideContainer"
+                                        : "")
+                                }
+                                key={key}
+                            >
+                                {expanded && (
+                                    <div className="studentDashboardSubjectDetailsContainer">
+                                        <div className="studentDashboardSubjectDetailsHeaderContainer">
+                                            <p className="studentDashboardSubjectDetailsTitle">
+                                                {subject.name}
+                                            </p>
+                                            <div
+                                                className="studentDashboardSubjectDetailsIconContainer"
+                                                onClick={() => {
+                                                    const index =
+                                                        expandedSubjects.indexOf(
+                                                            id
+                                                        );
+                                                    if (index !== -1) {
+                                                        const result =
+                                                            expandedSubjects.filter(
+                                                                (item) => {
+                                                                    return (
+                                                                        item !==
+                                                                        id
+                                                                    );
+                                                                }
+                                                            );
+                                                        setExpandedSubjects(
+                                                            result
+                                                        );
+                                                    }
+                                                }}
+                                            >
+                                                <SVGIcon
+                                                    id={"close"}
+                                                    className={
+                                                        "studentDashboardSubjectDetailsCloseIcon"
+                                                    }
+                                                ></SVGIcon>
+                                            </div>
+                                        </div>
+                                    </div>
+                                )}
                             </div>
                         );
                     })}
