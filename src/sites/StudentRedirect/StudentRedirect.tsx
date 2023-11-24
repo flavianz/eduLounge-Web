8376439495a3bcf3e1fd05bb1c99e4411433e17a
Flavianthepavian <flavianzuellig@hotmail.com>
2023-11-24 13:41:12 +0100

    Change route

--- src/sites/StudentRedirect/StudentRedirect.tsx
+++ src/sites/StudentRedirect/StudentRedirect.tsx
@@ -1,5 +1,5 @@
 import { Navigate } from "react-router";
 
 export default function StudentRedirect() {
-    return <Navigate to={"/students/login"} />;
+    return <Navigate to={"/students/dashboard"} />;
 }
