fc7dfdf7c3a5a99ad0c96fa7121a55712fb6b40d
Flavianthepavian <flavianzuellig@hotmail.com>
2023-11-24 13:40:43 +0100

    Finish subjects page

--- src/sites/StudentDashboard/StudentDashboardSubjects/StudentDashboardSubjects.tsx
+++ src/sites/StudentDashboard/StudentDashboardSubjects/StudentDashboardSubjects.tsx
@@ -5,8 +5,10 @@ import { subjects } from "../../../scripts/dummy";
 import SVGIcon from "../../../assets/images/svg/svg";
 import { useState } from "react";
 import { useNavigate } from "react-router";
+import { findAllByDisplayValue } from "@testing-library/react";
 
 export default function StudentDashboardOverview() {
+    document.title = "Noten | eduLounge";
     const [expandedSubjects, setExpandedSubjects] = useState<string[]>([]);
     const navigate = useNavigate();
     return (
@@ -31,11 +33,14 @@ export default function StudentDashboardOverview() {
                                 <p className="studentDashboardSubjectName">
                                     {subject.name}
                                 </p>
+                                <p className={"studentDashboardSubjectAverage"}>
+                                    {subject.average}
+                                </p>
                                 <div
                                     onClick={() => {
                                         navigate(
                                             "/students/dashboard/subjects/details?s=" +
-                                                id
+                                                id,
                                         );
                                     }}
                                     className="studentDashboardSubjectDetailsIconContainer"
@@ -47,16 +52,13 @@ export default function StudentDashboardOverview() {
                                         }
                                     />
                                 </div>
-                                <p className={"studentDashboardSubjectAverage"}>
-                                    {subject.average}
-                                </p>
                             </div>
                         ) : (
                             <div
                                 className={
-                                    "studentDashboardSubjectContainer" +
+                                    "studentDashboardSubjectWideContainer" +
                                     (expanded
-                                        ? " studentDashboardSubjectWideContainer"
+                                        ? " studentDashboardSubjectContainer"
                                         : "")
                                 }
                                 key={key}
@@ -68,35 +70,170 @@ export default function StudentDashboardOverview() {
                                                 {subject.name}
                                             </p>
                                             <div
-                                                className="studentDashboardSubjectDetailsIconContainer"
-                                                onClick={() => {
-                                                    const index =
-                                                        expandedSubjects.indexOf(
-                                                            id
-                                                        );
-                                                    if (index !== -1) {
-                                                        const result =
-                                                            expandedSubjects.filter(
-                                                                (item) => {
-                                                                    return (
-                                                                        item !==
-                                                                        id
-                                                                    );
-                                                                }
-                                                            );
-                                                        setExpandedSubjects(
-                                                            result
-                                                        );
-                                                    }
-                                                }}
+                                                className={
+                                                    "studentDashboardSubjectDetailsIconsExpanded"
+                                                }
                                             >
-                                                <SVGIcon
-                                                    id={"close"}
-                                                    className={
-                                                        "studentDashboardSubjectDetailsCloseIcon"
-                                                    }
-                                                ></SVGIcon>
+                                                <div
+                                                    className="studentDashboardSubjectDetailsIconExpanded"
+                                                    onClick={() => {
+                                                        navigate(
+                                                            "/students/dashboard/subjects/details?s=" +
+                                                                id,
+                                                        );
+                                                    }}
+                                                >
+                                                    <p
+                                                        className={
+                                                            "studentDashboardSubjectDetailsIconExpandedText"
+                                                        }
+                                                    >
+                                                        Details
+                                                    </p>
+                                                    <SVGIcon
+                                                        id={"details"}
+                                                        className={
+                                                            "studentDashboardSubjectDetailsIconExpandedIcon"
+                                                        }
+                                                    />
+                                                </div>
+                                                <div
+                                                    className="studentDashboardSubjectDetailsIconContainer"
+                                                    onClick={() => {
+                                                        const index =
+                                                            expandedSubjects.indexOf(
+                                                                id,
+                                                            );
+                                                        if (index !== -1) {
+                                                            const result =
+                                                                expandedSubjects.filter(
+                                                                    (item) => {
+                                                                        return (
+                                                                            item !==
+                                                                            id
+                                                                        );
+                                                                    },
+                                                                );
+                                                            setExpandedSubjects(
+                                                                result,
+                                                            );
+                                                        }
+                                                    }}
+                                                >
+                                                    <SVGIcon
+                                                        id={"close"}
+                                                        className={
+                                                            "studentDashboardSubjectDetailsCloseIcon"
+                                                        }
+                                                    ></SVGIcon>
+                                                </div>
                                             </div>
+                                        </div>
+                                        <div className="studentDashboardSubjectDetailsDataContainer">
+                                            <div
+                                                className={
+                                                    "studentDashboardSubjectDetailsTitleTableRowContainer"
+                                                }
+                                                key={key}
+                                            >
+                                                <p
+                                                    className={
+                                                        "studentDashboardSubjectDetailsTableHeader"
+                                                    }
+                                                    style={{ flex: 2 }}
+                                                >
+                                                    Datum
+                                                </p>
+                                                <p
+                                                    className={
+                                                        "studentDashboardSubjectDetailsTableHeader"
+                                                    }
+                                                    style={{ flex: 4 }}
+                                                >
+                                                    Thema
+                                                </p>
+                                                <p
+                                                    className={
+                                                        "studentDashboardSubjectDetailsTableHeader"
+                                                    }
+                                                    style={{ flex: 1 }}
+                                                >
+                                                    Note
+                                                </p>
+                                                <p
+                                                    className={
+                                                        "studentDashboardSubjectDetailsTableHeader"
+                                                    }
+                                                    style={{ flex: 1 }}
+                                                >
+                                                    Gewichtung
+                                                </p>
+                                            </div>
+                                            {Object.keys(subject.tests).map(
+                                                (testID, key) => {
+                                                    const test =
+                                                        subject.tests[testID];
+                                                    return (
+                                                        <a
+                                                            className={
+                                                                "studentDashboardSubjectDetailsTableRowContainer"
+                                                            }
+                                                            key={key}
+                                                            href={
+                                                                "/students/dashboard/subjects/mark?m=" +
+                                                                testID
+                                                            }
+                                                        >
+                                                            <p
+                                                                className={
+                                                                    "studentDashboardSubjectDetailsTableDate"
+                                                                }
+                                                            >
+                                                                {new Date(
+                                                                    test.start,
+                                                                ).toLocaleDateString()}
+                                                            </p>
+                                                            <p
+                                                                className={
+                                                                    "studentDashboardSubjectDetailsTableName"
+                                                                }
+                                                            >
+                                                                {test.name}
+                                                            </p>
+                                                            <p className="studentDashboardSubjectDetailsTableMark">
+                                                                {test.mark}
+                                                            </p>
+                                                            <p className="studentDashboardSubjectDetailsTableWeight">
+                                                                {test.weight}
+                                                            </p>
+                                                        </a>
+                                                    );
+                                                },
+                                            )}
+                                            <a
+                                                className="studentDashboardSubjectDetailsAverageContainer"
+                                                href={
+                                                    "/students/dashboard/subjects/details?s=" +
+                                                    id
+                                                }
+                                            >
+                                                <div
+                                                    className={
+                                                        "studentDashboardSubjectDetailsTableDate"
+                                                    }
+                                                ></div>
+                                                <p
+                                                    className={
+                                                        "studentDashboardSubjectDetailsTableName"
+                                                    }
+                                                >
+                                                    Gesamtschnitt
+                                                </p>
+                                                <p className="studentDashboardSubjectDetailsTableMark">
+                                                    {subject.average}
+                                                </p>
+                                                <div className="studentDashboardSubjectDetailsTableWeight"></div>
+                                            </a>
                                         </div>
                                     </div>
                                 )}
