0beb86a90d13aa9667de5f62f1f4005a6edb9b1d
Flavianthepavian <survivallist.net@gmail.com>
2024-01-08 22:08:46 +0100

    add temporal button for testing jwt

--- src/sites/Home.tsx
+++ src/sites/Home.tsx
@@ -1,9 +1,18 @@
 import Navbar from "../components/Navbar/Navbar.tsx";
+import { Button } from "@mantine/core";
+import { testAccessToken } from "../scripts/server.ts";
 
 export default function Home() {
     return (
         <div>
             <Navbar></Navbar>
+            <Button
+                onClick={async () => {
+                    console.log(await testAccessToken());
+                }}
+            >
+                Test
+            </Button>
         </div>
     );
 }
