4f0bdec627794f38ee6c9fb0b8f50f93756fd27a
Flavianthepavian <survivallist.net@gmail.com>
2024-01-08 22:08:10 +0100

    remove stories examples

--- src/sites/Login.tsx
+++ src/sites/Login.tsx
@@ -1,27 +1,40 @@
 import { useState } from "react";
 import { Button } from "@mantine/core";
+import { login } from "../scripts/server.ts";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const navigate = useNavigate();
 
-    function submit() {}
+    async function submit(event: { preventDefault: () => void }) {
+        event.preventDefault();
+        const result = await login(username, password);
+        if (result === 200) {
+            navigate("/home");
+        }
+    }
 
     return (
         <div>
-            <input
-                type="text"
-                value={username}
-                onChange={(event) => setUsername(event.target.value)}
-                placeholder={"Username"}
-            />
-            <input
-                type="text"
-                value={password}
-                onChange={(event) => setPassword(event.target.value)}
-                placeholder={"Password"}
-            />
-            <Button onClick={submit}>Einloggen</Button>
+            <form onSubmit={submit}>
+                <input
+                    type="text"
+                    value={username}
+                    onChange={(event) => setUsername(event.target.value)}
+                    placeholder={"Username"}
+                />
+                <input
+                    type="text"
+                    value={password}
+                    onChange={(event) => setPassword(event.target.value)}
+                    placeholder={"Password"}
+                />
+                <Button type="submit" onClick={submit}>
+                    Einloggen
+                </Button>
+            </form>
         </div>
     );
 }
