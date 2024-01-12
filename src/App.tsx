7d3c5984e66667f1460a08771d2f05601c8c30eb
Flavianthepavian <flavianzuellig@hotmail.com>
2024-01-12 14:46:11 +0100

    add react query

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,21 @@ import { MantineProvider } from "@mantine/core";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./sites/Home.tsx";
 import Login from "./sites/Login.tsx";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
 function App() {
     return (
         <MantineProvider>
-            <BrowserRouter>
-                <Routes>
-                    <Route path={"/"}>
-                        <Route path={"home"} element={<Home />} />
-                        <Route path={"login"} element={<Login />} />
-                    </Route>
-                </Routes>
-            </BrowserRouter>
+            <QueryClientProvider client={queryClient}>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path={"/"}>
+                            <Route path={"home"} element={<Home />} />
+                            <Route path={"login"} element={<Login />} />
+                        </Route>
+                    </Routes>
+                </BrowserRouter>
+            </QueryClientProvider>
         </MantineProvider>
     );
 }
